f854068df252a31888aa7a67eea92216c79425c2
Baptiste Becmeur <baptistebecmeur@gmail.com>
2015-11-24 16:39:39 +0100

    signup users done

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,5 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true, uniqueness: true
   validates :last_name, presence: true, uniqueness: true
-  validates :phone_number, presence: true
-  validates :address, presence: true
-  validates :availability, presence: true
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :medias
   end
 
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   root to: 'pages#home'
 
 end
