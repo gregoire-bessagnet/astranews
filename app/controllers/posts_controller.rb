bd58cb783c4ec8bd80892e8281678e8787e300f8
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-24 17:07:30 +0100

    controller for form post changed

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+
+before_filter :authenticate_user!, except: [:index, :show, :new, :create]
+before_action :find_post, only: [:index, :new, :create, :show, :destroy]
+
+
+  def index
+     @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+
+  end
+
+  def update
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :introduction, :content, :synopsis, :city, :category, :price, :licence)
+  end
+
+end
+
