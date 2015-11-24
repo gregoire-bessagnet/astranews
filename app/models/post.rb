d481082f15c837e76f7e463829a930ece9973590
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-24 11:26:22 +0100

    validates model Post done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,16 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
+  CATEGORIES = ["Société", "Culture", "Science", "Arts", "Médias", "Finance", "Economie"]
+
   validates :user_id, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :introduction, presence: true
+  validates :content, presence: true
+  validates :synopsis, length: {minimum:200}, presence: true
+  validates :city, presence: true
+  validates :category, inclusion: { in: Post::CATEGORIES, allow_nil: false }
+  validates :price, presence: true
+  validates :licence, presence: true
 
 end
