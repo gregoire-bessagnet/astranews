d2fb04f599b0a911f4a2714d48ca1020cb2f462d
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-25 12:35:24 +0100

    application controller updated with pundit settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  private
+
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(root_path)
