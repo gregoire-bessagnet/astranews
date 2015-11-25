b3fa1b6ce42615ae894e64b52092ac5a06a65043
Baptiste Becmeur <baptistebecmeur@gmail.com>
2015-11-25 17:56:56 +0100

    column cover to post added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125140411) do
+ActiveRecord::Schema.define(version: 20151125165508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20151125140411) do
     t.integer  "user_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "cover"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
