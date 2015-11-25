949a828a642c97da63bf12f84bfc2ee83293f001
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-25 14:58:05 +0100

    routes for FB done

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :medias
   end
 
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  devise_for :users, :controllers => { registrations: 'users/registrations' , omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :users, only: :show
   root to: 'pages#home'
 
