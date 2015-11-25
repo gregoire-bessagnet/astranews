b6aa106fd0fad384a289bc84b85774dc6c2efd39
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-25 12:36:58 +0100

    post policy updated with pundit settings

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,7 +1,25 @@
 class PostPolicy < ApplicationPolicy
+
   class Scope < Scope
     def resolve
-      scope
+      scope.all
     end
   end
+
+def show?
+  true
+end
+
+def create?
+  record.user == user
+end
+
+def update?
+  record.user == user
+end
+
+def destroy?
+  record.user == user
+end
+
 end
