16c0aeb044ff13adfbc39e76fc4c918b19edbb50
Baptiste Becmeur <baptistebecmeur@gmail.com>
2015-11-26 16:14:37 +0100

    ajout de joujous dans la navbar

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, :controllers => { registrations: 'users/registrations' , omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :users, only: :show
+  resources :users, only: [:show, :index]
 
 end
 
