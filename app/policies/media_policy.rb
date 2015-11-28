5e6aedc4958782e28735e5b745a44c96bedc0b5a
Baptiste Becmeur <baptistebecmeur@gmail.com>
2015-11-28 17:45:57 +0100

    Création view media en cours

---
+++ app/policies/media_policy.rb
@@ -0,0 +1,31 @@
+class MediaPolicy < ApplicationPolicy
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    is_owner?
+  end
+
+  def destroy?
+    is_owner?
+  end
+
+  private
+
+  def is_owner?
+    record.user == user
+  end
+
+end
