047a1691b11ca210b90c5b8d65b2a2c4c17e1907
Raphael Biondi <raf.biondi@gmail.com>
2015-12-01 13:38:20 +0100

    registre

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130110819) do
+ActiveRecord::Schema.define(version: 20151201122349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,13 @@ ActiveRecord::Schema.define(version: 20151130110819) do
     t.float    "price"
     t.string   "licence"
     t.integer  "user_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
     t.string   "cover"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
