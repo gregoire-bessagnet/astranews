8749858e5117b55d85a77388655c455f76dc089a
Raphael Biondi <raf.biondi@gmail.com>
2015-12-02 17:04:24 +0100

    page#index creation d'article + style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
+
+    @posts = Post.limit(9)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -14,7 +14,6 @@ class Post < ActiveRecord::Base
   validates :city, presence: true
   validates :category, inclusion: { in: Post::CATEGORIES, allow_nil: false }
   validates :status, inclusion: { in: Post::STATUS, allow_nil: false }
-  validates :price_cents, presence: true
   validates :licence, presence: true, inclusion: { in: Post::LICENCE, allow_nil: false }
 
   mount_uploader :cover, ImageUploader
