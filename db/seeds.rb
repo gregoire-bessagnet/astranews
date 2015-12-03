5288340461d778d98877a19df8a3491080c2415d
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-12-03 12:00:23 +0100

    coucou

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,7 +68,7 @@ puts "----Seeding Posts"
     Post.create({
       title: Faker::Lorem.sentence,
       introduction: Faker::Lorem.paragraph,
-      content: Faker::Lorem.paragraphs(3),
+      content: Faker::Lorem.paragraphs(5),
       date: Faker::Date.backward(90),
       synopsis: Faker::Lorem.sentence(2, true, 8),
       status:["Publié", "Brouillon"].sample,
